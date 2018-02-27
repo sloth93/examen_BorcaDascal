47cf64717b4d1f7d95f6624afd0934427a1a2f71
sloth93 <atox93@gmail.com>
2018-02-27 09:36:36 +0100

    primera pujada

---
+++ examen_BorcaDascal/src/examen_BorcaDascal/examenBorcaDascal.java
@@ -0,0 +1,22 @@
+package examen_BorcaDascal;
+
+public class examenBorcaDascal 
+{
+	public static void main (String[] args)
+	{
+		printOne();
+		printOne();
+		printTwo();
+	}
+	
+	public static void printOne()
+	{
+		System.out.println("Hello World");
+	}
+	
+	public static void printTwo ()
+	{
+		printOne();
+		printOne();
+	}
+}
