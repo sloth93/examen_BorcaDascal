ca2ba67643693ad59f1fb16c4ecb8b5493d642b2
sloth93 <atox93@gmail.com>
2018-02-27 09:42:34 +0100

    segona pujada

--- examen_BorcaDascal/src/examen_BorcaDascal/examenBorcaDascal.java
+++ examen_BorcaDascal/src/examen_BorcaDascal/examenBorcaDascal.java
@@ -5,18 +5,11 @@ public class examenBorcaDascal
 	public static void main (String[] args)
 	{
 		printOne();
-		printOne();
-		printTwo();
+		printOne();		
 	}
 	
 	public static void printOne()
 	{
 		System.out.println("Hello World");
-	}
-	
-	public static void printTwo ()
-	{
-		printOne();
-		printOne();
-	}
+	}	
 }
